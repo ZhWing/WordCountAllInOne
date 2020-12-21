963ec108d6a74d6414af636c26ef2038d999e244
Ivan Chien <qyc027@gmail.com>
2020-12-21 23:43:36 +0800

    Add Python solution

---
+++ WordCount.py
@@ -0,0 +1,23 @@
+import sys
+
+if __name__ == '__main__':
+
+  argv = sys.argv
+  argc = len(sys.argv)
+
+  if argc != 2:
+    print("Usage: %s [%s]" % (sys.argv[0], 'input-file'))
+    exit(1)
+
+  counter = dict()
+  
+  with open(sys.argv[1], 'r') as fp:
+    lines = fp.readlines()
+    for line in lines:
+      words = filter(lambda x: x, line.strip().split(','))
+      for word in words:
+        counter.setdefault(word, 0)
+        counter[word] += 1
+  
+  for k, v in counter.items():
+    print('%s %d' % (k, v))
